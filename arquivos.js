acaf90ff0ea0529a5d08a83ad403ba0c4e83abbb
Beatriz Mendes <bmendesgs@gmail.com>
2018-07-26 11:11:34 -0300

    Adicionar comandos de leitura e escrita

--- arquivos.js
+++ arquivos.js
@@ -1,18 +1,37 @@
 const fs= require("fs");
 
-function tratarleitura(erro, sucesso){
+/// Exemplo de leitura
+
+let texto;
+
+fs.readFile('teste.txt', function(erro, sucesso){
     if(erro){
-        console.log("Ihh, deu erro");
+        console.log("ERRO AO LER O ARQUIVO");
         console.log(erro);
     }
     else{
-        sucesso=sucesso.toJSON();
+        sucesso = sucesso.toString();
         console.log("Arquivo lido com sucesso");
         console.log(sucesso);
+        texto = sucesso;
     }
-}
+});
 
-let texto = fs.readFile('texte.txt', tratarleitura);
+console.log("Chamada de leitura executada com sucesso.");
 
-console.log("Função executada com sucesso");
-console.log(texto);
+
+// Exemplo de escrita de arquivo
+
+let paraEscrever = process.argv[2];
+
+// Para gravar sempre um novo arquivo, usar writeFile
+fs.appendFile('testeEscrita.txt', `${paraEscrever}\r\n`, erro => {
+    if(erro){
+        console.log("ERRO AO ESCREVER O ARQUIVO");
+    }
+    else{
+        console.log("Deu bom - arquivo escrito com sucesso");
+    }
+});
+
+console.log("Chamada de escrita executada com sucesso");
